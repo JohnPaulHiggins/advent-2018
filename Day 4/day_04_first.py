e08d87850126965f09cbae2a8b3ac0a652d6918d
Johnathan Higgins <Jhiggins@lifeline.local>
2018-12-12 16:50:07 -0500

    Improve assertions

--- Day 4/day_04_first.py
+++ Day 4/day_04_first.py
@@ -7,8 +7,6 @@ def parse_entry(entry):
     return re.match(m, entry).groups()
 
 
-print(parse_entry('[1518-07-03 23:58]'\
-                   'Guard #2437 begins shift'))
 assert parse_entry('[1518-07-03 23:58]'\
                    'Guard #2437 begins shift') == ('1518',
                                                    '07',
@@ -23,6 +21,7 @@ assert parse_entry('[1518-09-23 00:54] falls asleep') == ('1518',
                                                           '23',
                                                           '00',
                                                           '54',
+                                                          '',
                                                           'asleep')
 
 assert parse_entry('[1518-07-27 00:59] wakes up') == ('1518',
@@ -30,6 +29,7 @@ assert parse_entry('[1518-07-27 00:59] wakes up') == ('1518',
                                                       '27',
                                                       '00',
                                                       '59',
+                                                      '',
                                                       'up')
 
 assert sorted(['[1518-07-03 23:58] Guard #2437 begins shift',
